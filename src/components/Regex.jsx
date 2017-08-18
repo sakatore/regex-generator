edbda410c29b74b3f2c31bd972412097f758ae8f
Kyohei Sakai <nico_f00tb@yahoo.co.jp>
2017-08-18 19:17:57 +0900

    Add Regex component

---
+++ src/components/Regex.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import './Regex.css';
+
+export default class Regex extends Component {
+  render() {
+    return (
+      <div className="regex">
+        Regex
+      </div>
+    )
+  }
+}
