17e97ca216a9a4f094e336f3e26433a114ab0922
Kyohei Sakai <nico_f00tb@yahoo.co.jp>
2017-08-19 13:37:50 +0900

    Add selector and characters field

--- src/components/SelectableRegex.jsx
+++ src/components/SelectableRegex.jsx
@@ -3,11 +3,47 @@ import PropTypes from 'prop-types';
 import './SelectableRegex.css';
 
 export default class SelectableRegex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: '[]',
+      characters: ''
+    };
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.getCharacters = this.getCharacters.bind(this);
+  }
+
+  handleSelectChange(e) {
+    this.setState({type: e.target.value});
+  }
+
+  handleInputChange(e) {
+    this.setState({characters: e.target.value})
+  }
+
+  getCharacters() {
+    return (<input onChange={this.handleInputChange} value={this.state.text} />);
+  }
+
   getSelectors() {
     const { tabsCount } = this.props;
     const list = [];
     for (var i = 0; i < tabsCount; i++) {
-      list.push(<li>{i}</li>);
+      list.push(
+        <li>
+          <div>
+            <select onChange={this.handleSelectChange}>
+              <option value="Text">Text</option>
+              <option value="[]">[]</option>
+              <option value=".">.</option>
+              <option value="^">^</option>
+              <option value="$">$</option>
+            </select>
+          </div>
+          {this.getCharacters()}
+        </li>
+      );
     }
     return (
       <ul>
