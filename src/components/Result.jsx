4acf9138c086809a54da15f3830ae9c708aa0d62
Kyohei Sakai <nico_f00tb@yahoo.co.jp>
2017-08-23 10:19:32 +0900

    FIx style result field

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -6,7 +6,10 @@ export default class Result extends Component {
   render() {
     return (
       <div className="result">
-        <div className="field">Regex: {this.props.regex}</div>
+        <p1 className="title">Result</p1>
+        <div className="field">
+          <span className="regex">{this.props.regex}</span>
+        </div>
       </div>
     );
   }
